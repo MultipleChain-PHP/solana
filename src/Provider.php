2559e6efcde84f37feebceeccc9d08bb967e708f
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-06-11 15:38:16 +0800

    provider ready for dev

--- src/Provider.php
+++ src/Provider.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace MultipleChain\Solana;
 
 use MultipleChain\Enums\ErrorType;
+use MultipleChain\SolanaSDK\Connection;
+use MultipleChain\SolanaSDK\SolanaRpcClient;
 use MultipleChain\Interfaces\ProviderInterface;
 use MultipleChain\BaseNetworkConfig as NetworkConfig;
 
 class Provider implements ProviderInterface
 {
+    /**
+     * @var Connection
+     */
+    public Connection $web3;
+
     /**
      * @var NetworkConfig
      */
@@ -19,6 +26,32 @@ class Provider implements ProviderInterface
      * @var Provider|null
      */
     private static ?Provider $instance;
+
+    /**
+     *
+     * @var array<mixed>
+     */
+    public array $nodes = [
+        'mainnet' => [
+            'name' => 'Mainnet',
+            'cluster' => 'mainnet-beta',
+            'rpcUrl' => 'https://api.mainnet-beta.solana.com/',
+            'explorerUrl' => 'https://solscan.io/',
+            'wsUrl' => null
+        ],
+        'devnet' => [
+            'name' => 'Devnet',
+            'cluster' => 'devnet',
+            'rpcUrl' => 'https://api.devnet.solana.com/',
+            'explorerUrl' => 'https://solscan.io/',
+            'wsUrl' => null
+        ]
+    ];
+
+    /**
+     * @var array<string>
+     */
+    public array $node;
 
     /**
      * @param array<string,mixed> $network
@@ -59,6 +92,10 @@ class Provider implements ProviderInterface
     {
         self::$instance = $this;
         $this->network = new NetworkConfig($network);
+        $this->node = $this->nodes[$this->network->isTestnet() ? 'devnet' : 'mainnet'];
+        $this->node['rpcUrl'] = $this->network->getRpcUrl() ?? $this->node['rpcUrl'];
+        $this->node['wsUrl'] = $this->network->getWsUrl() ?? $this->node['wsUrl'];
+        $this->web3 = new Connection(new SolanaRpcClient($this->node['rpcUrl']));
     }
 
     /**
@@ -75,7 +112,26 @@ class Provider implements ProviderInterface
      */
     public function checkRpcConnection(?string $url = null): bool
     {
-        return true;
+        try {
+            $curl = curl_init($url ?? $this->node['rpcUrl']);
+            if (false === $curl) {
+                return false;
+            }
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode([
+                'jsonrpc' => '2.0',
+                'method' => 'getEpochInfo',
+                'id' => 1,
+            ]));
+            curl_exec($curl);
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            curl_close($curl);
+            return 200 === $httpCode;
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 
     /**
