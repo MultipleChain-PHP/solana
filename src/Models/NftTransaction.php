0a13f1af3a915d8f6a369508c119b32f22c1d026
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-06-11 19:07:00 +0800

    NftTransaction done

--- src/Models/NftTransaction.php
+++ src/Models/NftTransaction.php
@@ -6,16 +6,85 @@ namespace MultipleChain\Solana\Models;
 
 use MultipleChain\Enums\AssetDirection;
 use MultipleChain\Enums\TransactionStatus;
+use MultipleChain\SolanaSDK\Types\ParsedInstruction;
+use MultipleChain\SolanaSDK\Types\ParsedTransactionWithMeta;
 use MultipleChain\Interfaces\Models\NftTransactionInterface;
 
 class NftTransaction extends ContractTransaction implements NftTransactionInterface
 {
     /**
+     * @param ParsedTransactionWithMeta $data
+     * @return ParsedInstruction|null
+     */
+    private function findTransferInstruction(ParsedTransactionWithMeta $data): ?ParsedInstruction
+    {
+        return array_reduce($data->getTransaction()->getMessage()->getInstructions(), function ($carry, $instruction) {
+            /**
+             * @var ParsedInstruction $instruction
+             */
+            if (
+                null !== $instruction->getParsed()
+                && in_array($instruction->getParsed()['type'], ['transfer', 'transferChecked'])
+            ) {
+                return $instruction;
+            }
+            return $carry;
+        }, null);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        $parsed = $this->findTransferInstruction($data)?->getParsed();
+
+        if (null === $parsed) {
+            return '';
+        }
+
+        if (isset($parsed['info']['mint'])) {
+            return $parsed['info']['mint'];
+        }
+
+        $postBalance = array_reduce($data->getMeta()->getPostTokenBalances(), function ($carry, $balance) {
+            if (isset($balance['mint'])) {
+                return $balance;
+            }
+            return $carry;
+        }, null);
+
+        if (null !== $postBalance) {
+            return $postBalance['mint'];
+        }
+
+        return parent::getAddress();
+    }
+
+    /**
      * @return string
      */
     public function getReceiver(): string
     {
-        return '0x';
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        $balances = $data->getMeta()->getPostTokenBalances();
+
+        if (null === $balances || 0 === count($balances)) {
+            return '';
+        }
+
+        return $balances[0]->getOwner() ?? '';
     }
 
     /**
@@ -23,7 +92,19 @@ class NftTransaction extends ContractTransaction implements NftTransactionInterf
      */
     public function getSender(): string
     {
-        return '0x';
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        $parsed = $this->findTransferInstruction($data)?->getParsed();
+
+        if (null === $parsed) {
+            return '';
+        }
+
+        return $parsed['info']['authority'];
     }
 
     /**
@@ -31,7 +112,19 @@ class NftTransaction extends ContractTransaction implements NftTransactionInterf
      */
     public function getNftId(): int|string
     {
-        return 0;
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        $parsed = $this->findTransferInstruction($data)?->getParsed();
+
+        if (null === $parsed) {
+            return '';
+        }
+
+        return $parsed['info']['mint'];
     }
 
     /**
@@ -42,6 +135,26 @@ class NftTransaction extends ContractTransaction implements NftTransactionInterf
      */
     public function verifyTransfer(AssetDirection $direction, string $address, int|string $nftId): TransactionStatus
     {
-        return TransactionStatus::PENDING;
+        $status = $this->getStatus();
+
+        if (TransactionStatus::PENDING === $status) {
+            return TransactionStatus::PENDING;
+        }
+
+        if ($this->getNftId() !== $nftId) {
+            return TransactionStatus::FAILED;
+        }
+
+        if (AssetDirection::INCOMING === $direction) {
+            if (strtolower($this->getReceiver()) !== strtolower($address)) {
+                return TransactionStatus::FAILED;
+            }
+        } else {
+            if (strtolower($this->getSender()) !== strtolower($address)) {
+                return TransactionStatus::FAILED;
+            }
+        }
+
+        return TransactionStatus::CONFIRMED;
     }
 }
