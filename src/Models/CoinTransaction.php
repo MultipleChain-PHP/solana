f4c7fecf55854978e4622a784a901db472054781
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-06-11 18:22:59 +0800

    CoinTransaction done

--- src/Models/CoinTransaction.php
+++ src/Models/CoinTransaction.php
@@ -5,18 +5,54 @@ declare(strict_types=1);
 namespace MultipleChain\Solana\Models;
 
 use MultipleChain\Utils\Number;
+use MultipleChain\Solana\Utils;
+use MultipleChain\Solana\Assets\Coin;
 use MultipleChain\Enums\AssetDirection;
 use MultipleChain\Enums\TransactionStatus;
+use MultipleChain\SolanaSDK\Types\ParsedInstruction;
+use MultipleChain\SolanaSDK\Types\ParsedTransactionWithMeta;
 use MultipleChain\Interfaces\Models\CoinTransactionInterface;
 
 class CoinTransaction extends Transaction implements CoinTransactionInterface
 {
     /**
+     * @param ParsedTransactionWithMeta $data
+     * @return ParsedInstruction|null
+     */
+    private function findTransferInstruction(ParsedTransactionWithMeta $data): ?ParsedInstruction
+    {
+        return array_reduce($data->getTransaction()->getMessage()->getInstructions(), function ($carry, $instruction) {
+            /**
+             * @var ParsedInstruction $instruction
+             */
+            if (
+                null !== $instruction->getParsed()
+                && in_array($instruction->getParsed()['type'], ['transfer', 'createAccount'])
+            ) {
+                return $instruction;
+            }
+            return $carry;
+        }, null);
+    }
+
+    /**
      * @return string
      */
     public function getReceiver(): string
     {
-        return '0x';
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        $parsed = $this->findTransferInstruction($data)?->getParsed();
+
+        if (null === $parsed) {
+            return '';
+        }
+
+        return $parsed['info']['destination'] ?? $parsed['info']['newAccount'] ?? '';
     }
 
     /**
@@ -24,7 +60,13 @@ class CoinTransaction extends Transaction implements CoinTransactionInterface
      */
     public function getSender(): string
     {
-        return '0x';
+        $data = $this->getData();
+
+        if (null === $data) {
+            return '';
+        }
+
+        return $this->findTransferInstruction($data)?->getParsed()['info']['source'] ?? '';
     }
 
     /**
@@ -32,7 +74,21 @@ class CoinTransaction extends Transaction implements CoinTransactionInterface
      */
     public function getAmount(): Number
     {
-        return new Number('0');
+        $data = $this->getData();
+
+        if (null === $data) {
+            return new Number(0);
+        }
+
+        $instruction = $this->findTransferInstruction($data);
+
+        if (null === $instruction) {
+            return new Number(0);
+        }
+
+        $lamports = $instruction->getParsed()['info']['lamports'];
+
+        return new Number(Utils::fromLamports($lamports), (new Coin())->getDecimals());
     }
 
     /**
@@ -43,6 +99,26 @@ class CoinTransaction extends Transaction implements CoinTransactionInterface
      */
     public function verifyTransfer(AssetDirection $direction, string $address, float $amount): TransactionStatus
     {
-        return TransactionStatus::PENDING;
+        $status = $this->getStatus();
+
+        if (TransactionStatus::PENDING === $status) {
+            return TransactionStatus::PENDING;
+        }
+
+        if ($this->getAmount()->toFloat() !== $amount) {
+            return TransactionStatus::FAILED;
+        }
+
+        if (AssetDirection::INCOMING === $direction) {
+            if (strtolower($this->getReceiver()) !== strtolower($address)) {
+                return TransactionStatus::FAILED;
+            }
+        } else {
+            if (strtolower($this->getSender()) !== strtolower($address)) {
+                return TransactionStatus::FAILED;
+            }
+        }
+
+        return TransactionStatus::CONFIRMED;
     }
 }
